4135d800bfbd0fab9cfffe853dcfb6ff494531ae
NiltonMorais <nilton_even@hotmail.com>
2016-06-30 15:25:14 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$arrayPessoas = [];
+$arrayProfissoes = [];
+
+$con = new PDO("mysql:host=localhost;dbname=exemplo", "root", "root");
+
+$query1 = $con->query("SELECT * FROM pessoa"); // primeira query para pegar todas as pessoas
+
+while ($pessoa = $query1->fetch(PDO::FETCH_OBJ)) {
+
+    // segunda query para pegar todas as profissões da pessoa atual
+    $query2 = $con->query("select distinct * from pessoa_profissao inner join (profissao) where pessoa_id = " . $pessoa->id);
+
+    // coloca todas as profissões da pessoa atual em um array, onde a key é o id da profissão e o value é o nome da profissão
+    while ($profissao = $query2->fetch(PDO::FETCH_OBJ)) {
+        $arrayProfissoes[$profissao->id] = $profissao->profissao;
+    }
+
+    // adiciona um item no array, onde a key é o id da pessoa atual, o nome é o nome da pessoa e a profissao é um array com todas as profissões da pessoa atual
+    $arrayPessoas[$pessoa->id] = [
+        'nome' => $pessoa->pessoa,
+        'profissão' => $arrayProfissoes
+    ];
+    $arrayProfissoes = [];  // zera array de profissões
+}
+
+print_r($arrayPessoas);
